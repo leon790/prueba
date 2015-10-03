c83f42a86b1f98b5fbea3abc3577d03532945965
Edward Benitez <edeelectronic@gmail.com>
2015-10-03 11:14:45 -0500

    Canvas leo

--- archivo1.cpp
+++ archivo1.cpp
@@ -10,5 +10,7 @@ count= 5+a;
 catc(Exception e)
 {
   toast.maketext(getapplicationcontext, "no funciono el try", toast.LENG_SHORT).show;
+  print_pek <"Pek pek pek la banda">;
+  pek_if("La banda malandra");
 }
 }
