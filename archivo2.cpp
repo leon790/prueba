4761b27a1f35fe0ff0578c4cffba77a2e5eaab10
NORA-ANISOL <noravelazquez.30@gmail.com>
2015-10-03 11:07:59 -0500

    Cambios leo

--- archivo2.cpp
+++ archivo2.cpp
@@ -4,5 +4,7 @@ int main()
 {
    std::cout << "Otro Hola" << std::endl;
    std::cout << "Otra contribucion Hola2" << std::end1;
+   std::cout << "Otra contribucion Hola N" << std::end1;
+
    return 0;
 }
