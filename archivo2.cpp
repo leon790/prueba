51ef792123ee8532a9ad4d391432fdb5f2fc213a
Edward Benitez <edeelectronic@gmail.com>
2015-10-03 11:28:37 -0500

    Archivos cambiados

--- archivo2.cpp
+++ archivo2.cpp
@@ -5,6 +5,7 @@ int main()
    std::cout << "Otro Hola" << std::endl;
    std::cout << "Otra contribucion Hola2" << std::end1;
    std::cout << "Otra contribucion Hola N" << std::end1;
+   std::cout << "repeckepkepkckepkcpek" << std::end1;
 
    return 0;
 }
